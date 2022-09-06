31bc0a8a909bc464e0c9260f07b2544f56ceff11
Tyrone <tyrone.cloma0219@gmail.com>
2022-09-06 13:20:25 -0400

    commit tree 23a16017f4953e1daa0f5ba003112e0b23c5b763

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,12 @@
-export function manageFriends(state, action) {
-  // your code here
+let initialState = { friends: [] }
+
+export function manageFriends(state = initialState, action) {
+  switch (action.type) {
+    case 'friends/add':
+      return { friends: [...state.friends, action.payload] }
+    case 'friends/remove':
+      return { friends: [...state.friends.filter(friend => { friend.id !== action.payload })] }
+    default:
+      return state;
+  }
 }
